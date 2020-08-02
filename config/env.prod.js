3180e0d65caae6be20f0f680ad3cf44ef974d737
Anshul Sahni <anshul_sahni@live.com>
2020-08-02 16:51:24 +0530

    [CONF][FIX]:Fixed a type prod env file

--- config/env.prod.js
+++ config/env.prod.js
@@ -4,7 +4,7 @@ module.exports = {
     rzpKeySecret: process.env.TEAM1_RZP_KEY_SECRET,
   },
 
-  team: {
+  team2: {
     rzpKeyId: process.env.TEAM2_RZP_KEY_ID,
     rzpKeySecret: process.env.TEAM2_RZP_KEY_SECRET,
   },
